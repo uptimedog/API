a2cd0320348f880ea58e7f7eeb761df8112876ef
Clivern <hello@clivern.com>
2021-09-04 22:48:16 +0200

    update

--- mix.exs
+++ mix.exs
@@ -52,10 +52,7 @@ defmodule Rune.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.2", only: [:dev], runtime: false},
-      # Documentation dependencies
       {:ex_doc, "~> 0.28", optional: true, only: [:docs]}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
