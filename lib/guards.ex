40dee4b9d654203b3d8718e75aa32a27200c134d
Clivern <hello@clivern.com>
2021-09-07 19:19:54 +0200

    add elixir guards

---
+++ lib/guards.ex
@@ -0,0 +1,24 @@
+# Copyright 2022 Clivern. All rights reserved.
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE file.
+
+defmodule Rune.Guards do
+  # Custom Guard
+  defguard is_even(x) when is_integer(x) and rem(x, 2) == 0
+
+  def inspect_map(x) when is_map(x) and map_size(x) > 0 do
+    "x = #{inspect(x)}"
+  end
+
+  def non_zero(y) when y > 0 or y < 0 do
+    "y = #{inspect(y)}"
+  end
+
+  def test_1({:ok, value}) when value > 0 do
+    "value = #{inspect(value)}"
+  end
+
+  def test_2(y) when is_even(y) do
+    "y = #{inspect(y)}"
+  end
+end
