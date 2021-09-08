dc05457e1302815f30be980708a12fd9f00f00c1
Clivern <hello@clivern.com>
2021-09-09 00:50:35 +0200

    add uri

--- lib/uri.ex
+++ lib/uri.ex
@@ -3,12 +3,12 @@
 # license that can be found in the LICENSE file.
 
 defmodule Rune.URI do
-  def parse(url) do
+  def parse(url \\ "https://clivern.com") do
     uri = URI.parse(url)
     URI.to_string(uri)
   end
 
-  def get_uri_01(url) do
+  def get_uri_01(url \\ "https://clivern.com") do
     {status, uri} = URI.new(url)
 
     case status do
@@ -17,11 +17,19 @@ defmodule Rune.URI do
     end
   end
 
-  def get_uri_02(url) do
+  def get_uri_02(url \\ "https://clivern.com") do
     try do
       {:ok, URI.new!(url)}
     rescue
       _ -> {:error, ""}
     end
+  end
+
+  def query(uri \\ "foo=1&bar=2") do
+    URI.query_decoder(uri) |> Enum.to_list()
+  end
+
+  def port(protocol \\ "ftp") do
+    URI.default_port(protocol)
   end
 end
