0d85b9424eec7bdb4f29ff5de7bae6949429d136
Clivern <hello@clivern.com>
2022-04-06 17:47:37 +0200

    update

--- lib/maps.ex
+++ lib/maps.ex
@@ -4,12 +4,14 @@
 
 defmodule Rune.Maps do
   def test() do
+    # Docs https://hexdocs.pm/elixir/main/Map.html
     x = %{:name => "joe", :age => "four"}
     y = %{x | :name => "sam"}
     z = %{name: "joe", age: 15}
 
     IO.puts("#{inspect(y)}")
     IO.puts("#{inspect(z)}")
+    IO.puts("#{z[:name]}")
 
     # Keyword List
     # [primary: "red", secondary: "green"]
@@ -17,16 +19,34 @@ defmodule Rune.Maps do
     IO.puts("#{colors[:primary]}")
     IO.puts("#{colors[:secondary]}")
 
-    # map_size(x) # 2
-    # Map.filter(x, fn {k, _v} -> k == :age end)
-    # Map.get(x, :age)
-    # Map.get(x, :ag, "default")
-    # Map.has_key?(x, :ag) # false
-    # Map.has_key?(x, :age) # true
-  end
+    # map size
+    IO.puts map_size(x)
 
-  def where(klist) do
-    # Rune.Maps.where where: true == true, where: false == false # [where: true, where: true]
-    IO.puts("#{inspect(klist)}")
+    # map filter
+    z = Map.filter(x, fn {k, _v} -> k == :age end)
+    IO.puts("#{z[:age]}")
+
+    # get value by a key
+    IO.puts Map.get(x, :age)
+
+    # get a value by key
+    IO.puts Map.get(x, :ag, "default")
+
+    # check if a key exist
+    IO.puts Map.has_key?(x, :ag) # false
+
+    IO.puts Map.has_key?(x, :age) # true
+
+    case Map.fetch(%{a: 1}, :e) do
+      {:ok, val} -> IO.puts val
+      :error -> IO.puts "not found"
+    end
+
+    case Map.fetch(%{a: 1}, :a) do
+      {:ok, val} -> IO.puts val
+      :error -> IO.puts "not found"
+    end
+
+    Map.module_info
   end
 end
